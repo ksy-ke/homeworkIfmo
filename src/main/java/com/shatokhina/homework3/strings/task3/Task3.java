995ab55cd63e5c90004284e913b29c9a596d86a0
Shatokhina Kseniya <ksy6501@ya.ru>
2019-03-17 03:36:46 +0300

    homework 3, strings task 3 with stream

--- src/main/java/com/shatokhina/homework3/strings/task3/Task3.java
+++ src/main/java/com/shatokhina/homework3/strings/task3/Task3.java
@@ -1,21 +1,20 @@
 package com.shatokhina.homework3.strings.task3;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Matcher;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 public class Task3 {
 
+
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("\\d+");
-        String input = new Scanner(System.in).nextLine();
-
-        Matcher matcher = pattern.matcher(input);
-        List<String> numbers = new ArrayList<>();
-        while (matcher.find()) { numbers.add(matcher.group()); }
-
-        for (String current : numbers) { System.out.print(current + " "); }
-    }
+        var input = new Scanner(System.in).nextLine();
+        int[] numbers = Pattern.compile("\\d+").matcher(input)
+                .results()
+                .map(MatchResult::group)
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        System.out.println(Arrays.toString(numbers));
+   }
 }
